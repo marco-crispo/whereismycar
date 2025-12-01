8d9943461bd713017b10a9adcc7cd93596205186
Marco Crispo <marco.crispo.71@gmail.com>
2025-12-01 13:12:14 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'whereismycar',
+  webDir: 'dist'
+};
+
+export default config;
